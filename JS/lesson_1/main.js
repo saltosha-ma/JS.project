fbeb7871cd02f928d5b124d991a431936c747ec4
saltosha-ma <thesaltanat_1@mail.ru>
2024-08-03 12:48:07 +0600

    lesson-1

---
+++ JS/lesson_1/main.js
@@ -0,0 +1,35 @@
+// Регулярные выражения - Regular expressions
+
+// const username = prompt('Enter your name...')
+
+// const regExp = /n/ig
+
+// console.log(username.match(regExp));
+
+
+
+// const numbers = 'QWE123QWE456789QWE'
+
+// const regExp = /[0-9a-zA-Z]/g
+// const regExp = /\d/g
+// const regExp = /[\W]w]/g
+
+// console.log(numbers.replace(regExp, '*'));
+// console.log(numbers.match(regExp));
+
+
+
+
+// Recursion - Рекурсия 
+
+// console.log(numbers.match(regExp));
+
+// let num = 0
+// const counter = () => {
+//    num++
+//    console.log(num);
+//    if (num < 1000) {
+//       requestAnimationFrame(counter)
+//    }
+// }
+// counter()
